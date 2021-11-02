5b69d9305f9f150b373df32f30202b4439f07e7b
Vlad Dovgalyuk <ipz204_dvo@student.ztu.edu.ua>
2021-11-02 19:35:59 +0200

    init commit

---
+++ script.js
@@ -0,0 +1,60 @@
+// uah usd eur rub gbp - available currency
+// currency in usd
+let uah = 26.282;
+let usd = 1;
+let eur = 0.863;
+let gbp = 0.734;
+let rub = 71.79;
+
+let checker = true;
+
+while (checker) {
+
+    let currencyStr = prompt("Enter your current currency (uah, usd, eur, rub, gbp)");
+    // check if correct input
+    while (currencyStr != "uah" &&
+        currencyStr != "usd" &&
+        currencyStr != "eur" &&
+        currencyStr != "rub" &&
+        currencyStr != "gbp"
+    ) {
+        currencyStr = prompt("Error, try again. Enter currency (uah, usd, eur, rub, gbp):");
+    }
+
+    let currencyNum =
+        currencyStr == "uah" ? 26.282 :
+            currencyStr == "usd" ? 1 :
+                currencyStr == "eur" ? 0.863 :
+                    currencyStr == "gbp" ? 0.734 : 71.79;
+
+    let amount = prompt("Enter your amount of money:");
+    // check if correct input
+    while (isNaN(amount)) {
+        amount = prompt("Error, try again. Enter your amount of money:");
+    }
+
+
+    let convertToStr = prompt("Enter currency that you want to convert to (uah, usd, eur, rub, gbp):");
+    // check if correct input
+    while (convertToStr != "uah" &&
+        convertToStr != "usd" &&
+        convertToStr != "eur" &&
+        convertToStr != "rub" &&
+        convertToStr != "gbp"
+    ) {
+        convertToStr = prompt("Error, try again. Enter currency that you want to convert to (uah, usd, eur, rub, gbp):");
+    }
+
+    let convertToNum =
+        convertToStr == "uah" ? 26.282 :
+            convertToStr == "usd" ? 1 :
+                convertToStr == "eur" ? 0.863 :
+                    convertToStr == "gbp" ? 0.734 : 71.79;
+
+    let convertedWallet = +((amount / currencyNum) * convertToNum).toFixed(3);
+
+    alert(`Your ${amount}${currencyStr} = ${convertedWallet}${convertToStr}`);
+
+    checker = confirm("Repeat convertation?");
+}
+
